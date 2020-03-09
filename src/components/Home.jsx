5eed320cc39c682bf3bb1e42e719fc0af6423a8e
leer <ladesigns90@gmail.com>
2020-03-08 20:01:52 -0400

    created my search input and created my state with the array of objects of dummy data.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,53 @@
 import React, { Component } from 'react'
-
+import Search from './Search.jsx'
 export default class Home extends Component {
+    state = [
+        {
+          "name":"The Office",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/85/213184.jpg"
+        },
+        {
+          "name":"Radiant Office",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/101/254702.jpg"
+        },
+        {
+          "name":"The Office",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/93/234802.jpg"
+        },
+        {
+          "name":"Mr. Box Office",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/97/244942.jpg"
+        },
+        {
+          "name":"The Queen of Office",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/58/146476.jpg"
+        },
+        {
+          "name":"No Offence",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/48/121682.jpg"
+        },
+        {
+          "name":"Oficer",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/29/73047.jpg"
+        },
+        {
+          "name":"Trzeci oficer",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/29/73053.jpg"
+        },
+        {
+          "name":"Line Offline: Salaryman",
+          "image":"http://static.tvmaze.com/uploads/images/medium_portrait/57/143508.jpg"
+        },
+        {
+          "name":"Utenai Keikan","image":"http://static.tvmaze.com/uploads/images/medium_portrait/42/106093.jpg"
+        }
+      ]
+      
     render() {
         return (
             <div>
-                <h1>TVMaze React</h1>
+                <div class="ui center aligned container ui header">TVMaze React</div>
+                <Search />
             </div>
         )
     }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,8 +3,15 @@ import React, { Component } from 'react'
 export default class Search extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="ui segment">
+                <form className="ui form">
+                    <div className="field">
+                        <div className="ui massive icon input">
+                            <input type="text" placeholder="search..." />
+                            <i className="search icon"></i>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
